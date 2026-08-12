33e8b5e0817d6a01a0c2cccef9b9e81ff67a13ae
Glitchhh299 <tanmayofficials512@gmail.com>
2026-08-12 11:02:18 +0530

    Implement basic arithmetic operations in prog1day1.c
    Fix arithmetic operations and print statement.

---
+++ prog1day1.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+int main()
+{
+	int a,b;
+	int sum, sub, mul, div, rem;
+	printf("Enter Two Numbers: ");
+	scanf("%d%d",&a,&b);
+	printf("a=%d, b=%d\n",a,b);
+	sum = a+b;
+  printf("sum=%d, sum);
+return 0;
+}
